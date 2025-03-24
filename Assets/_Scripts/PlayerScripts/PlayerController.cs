fdd14229633c6d27953b1af608fdb09237d4fb78
NguyenMinh205 <nqm1312@gmail.com>
2025-03-24 22:19:13 +0700

    Update player animation

--- Assets/_Scripts/PlayerScripts/PlayerController.cs
+++ Assets/_Scripts/PlayerScripts/PlayerController.cs
@@ -31,17 +31,16 @@ public class PlayerController : Singleton<PlayerController>
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
         {
-            if (isGrounded)
+            if (isGrounded == true)
             {
                 playerMovement.Jump(jumpForce);
-                playerAnim.Animator.SetBool("IsGrounded", false);
                 canDoubleJump = true;
                 isGrounded = false;
             }
             else if (canDoubleJump)
             {
-                playerMovement.Jump(jumpForce);
                 playerAnim.PlayAnimDoubleJump();
+                playerMovement.Jump(jumpForce * 1.15f);
                 canDoubleJump = false;
             }
         }
@@ -51,10 +50,14 @@ public class PlayerController : Singleton<PlayerController>
     {
         isGrounded = CheckGround();
 
-        if (isGrounded)
+        if (isGrounded == true)
         {
             playerAnim.Animator.SetBool("IsGrounded", true);
             playerAnim.Animator.SetBool("DoubleJump", false);
+        }
+        else
+        {
+            playerAnim.Animator.SetBool("IsGrounded", false);
         }
     }
 
@@ -63,5 +66,23 @@ public class PlayerController : Singleton<PlayerController>
         RaycastHit2D hit = Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
         Debug.DrawRay(collider2D.bounds.center, Vector2.down * (collider2D.bounds.extents.y + extraHeight), Color.red);
         return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (TryGetComponent(out Collider2D collider))
+        {
+            Vector2 origin = (Vector2)collider2D.bounds.center;
+            Vector2 size = collider.bounds.size;
+            Vector2 direction = Vector2.down;
+            float distance = extraHeight;
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(origin, size);
+
+            Vector2 newPosition = origin + direction * distance;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(newPosition, size);
+        }
     }
 }
