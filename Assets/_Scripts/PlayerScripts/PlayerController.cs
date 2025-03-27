35deef5c8f9d9cfb7c55dd679eb78d8c6eda9ab5
NguyenMinh205 <nqm1312@gmail.com>
2025-03-27 15:18:11 +0700

    Optimize and add sprites

--- Assets/_Scripts/PlayerScripts/PlayerController.cs
+++ Assets/_Scripts/PlayerScripts/PlayerController.cs
@@ -25,9 +25,6 @@ public class PlayerController : Singleton<PlayerController>
     private void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        playerMovement.Move(horizontal, moveSpeed);
-        playerAnim.PlayAnimRun(horizontal);
-        playerAnim.PlayAnimJumpAndFall(playerMovement.Rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
         {
@@ -48,6 +45,9 @@ public class PlayerController : Singleton<PlayerController>
 
     private void FixedUpdate()
     {
+        playerMovement.Move(horizontal, moveSpeed);
+        playerAnim.PlayAnimRun(horizontal);
+        playerAnim.PlayAnimJumpAndFall(playerMovement.Rb.velocity.y);
         isGrounded = CheckGround();
 
         if (isGrounded == true)
@@ -64,7 +64,6 @@ public class PlayerController : Singleton<PlayerController>
     private bool CheckGround()
     {
         RaycastHit2D hit = Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
-        Debug.DrawRay(collider2D.bounds.center, Vector2.down * (collider2D.bounds.extents.y + extraHeight), Color.red);
         return hit.collider != null;
     }
 
