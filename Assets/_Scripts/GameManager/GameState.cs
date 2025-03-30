854b40afe5875385a12b0d70b04228f127fec9e4
NguyenMinh205 <nqm1312@gmail.com>
2025-03-30 15:27:14 +0700

    Add physic material 2D, script GameState and fix some bugs

---
+++ Assets/_Scripts/GameManager/GameState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    InStartScene,
+    WaitingChoiceLevel,
+    Playing,
+    Win,
+    Pause,
+    Lose
+}
